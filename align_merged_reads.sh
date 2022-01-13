05343eb6a0e0a9430494192abe8d194f8163baf7
peterciaccia <pnciaccia@gmail.com>
2022-01-13 15:32:30 -0500

    - moves additional env variable declarations to .env

--- align_merged_reads.sh
+++ align_merged_reads.sh
@@ -2,14 +2,14 @@
 
 source .env
 ANALYSIS_DIR="$1"
-REFERENCE_SEQ="${PROJECTPATH}/bwa_reference/indexed/reference_pseudoORF.fa"
 
 # index reference genome
 ${BWA_PATH} index "${REFERENCE_SEQ}"
 
 # aligns reads
 ${BWA_PATH} mem "${REFERENCE_SEQ}" "${ANALYSIS_DIR}/merged_reads.fastq" | \
-samtools sort -@8 -o "${ANALYSIS_DIR}/aligned_reads_sorted.bam" -
+  samtools sort -@8 -o "${ANALYSIS_DIR}/aligned_reads_sorted.bam" -
+samtools view -h -o "${ANALYSIS_DIR}/aligned_reads_sorted.sam" "${ANALYSIS_DIR}/aligned_reads_sorted.bam"
 
 # lists allelic frequencies
 bcftools mpileup -f "${REFERENCE_SEQ}" \
